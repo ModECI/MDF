a581422df678d28b192258c43898a556c23acfb3
Padraig Gleeson <p.gleeson@gmail.com>
2021-03-31 15:40:55 +0100

    For #32; testing with black formatting...

--- examples/abcd_python.py
+++ examples/abcd_python.py
@@ -1,9 +1,9 @@
-'''
+"""
 
 A simple set of nodes connected A->B->C->D, with one function at each, calculating
 inputs/outputs
 
-'''
+"""
 
 import math
 
@@ -14,33 +14,35 @@ A_intercept = 2.2
 
 # B - Logistic
 
-B_gain = 1.
-B_bias = 0.
-B_offset = 0.
+B_gain = 1.0
+B_bias = 0.0
+B_offset = 0.0
 
 # C - Exponential
 
-C_scale = 1.
-C_rate = 1.
-C_bias = 0.
-C_offset = 0.
+C_scale = 1.0
+C_rate = 1.0
+C_bias = 0.0
+C_offset = 0.0
 
 # D - Sine
 
-D_scale = 1.
+D_scale = 1.0
 
-test_values = [-1.,0.,1.,5.]
+test_values = [-1.0, 0.0, 1.0, 5.0]
+
 
 def evaluate(input):
     A = A_slope * input + A_intercept
-    B = 1/(1+math.exp(-1*B_gain*(A + B_bias)+B_offset))
+    B = 1 / (1 + math.exp(-1 * B_gain * (A + B_bias) + B_offset))
     C = C_scale * math.exp((C_rate * B) + C_bias) + C_offset
     D = D_scale * math.sin(C)
-    print('  Input value %s:\tA=%s,\tB=%s,\tC=%s,\tD=%s'%(input, A, B, C, D))
+    print("  Input value %s:\tA=%s,\tB=%s,\tC=%s,\tD=%s" % (input, A, B, C, D))
     return A, B, C, D
 
-if __name__ == '__main__':
 
-    print('Evaluating ABCD net in Python, with values %s'%test_values)
+if __name__ == "__main__":
+
+    print("Evaluating ABCD net in Python, with values %s" % test_values)
     for i in test_values:
         evaluate(i)
