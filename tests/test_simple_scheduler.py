c8cdf51eeda7e08201ba624f6764b35848f3cb60
Padraig Gleeson <p.gleeson@gmail.com>
2021-04-13 19:05:39 +0100

    Fix failing test

--- tests/test_simple_scheduler.py
+++ tests/test_simple_scheduler.py
@@ -1,6 +1,6 @@
 import pytest
 
-@pytest.mark.xfail(reason='Path issues with defaul JSON model.')
+@pytest.mark.xfail(reason='Path issues with default JSON model.')
 def test_simple_scheduler_main(tmpdir):
     import modeci_mdf.simple_scheduler
     modeci_mdf.simple_scheduler.main()
