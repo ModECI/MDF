84491bd16b97090f36de320df0f93042c00045d5
Katherine Mantel <kmantel@princeton.edu>
2021-04-20 21:55:24 -0400

    tests: add condition_scheduler tests

--- tests/test_schedulers.py
+++ tests/test_schedulers.py
@@ -7,3 +7,13 @@ def test_simple_scheduler_main(tmpdir):
 
     modeci_mdf.simple_scheduler.main("examples/Simple.json")
     modeci_mdf.simple_scheduler.main("examples/ABCD.yaml")
+
+
+@pytest.mark.parametrize(
+    "fi", ["examples/abc_conditions.json", "examples/abc_conditions.yaml"]
+)
+def test_condition_scheduler_main(fi):
+
+    import modeci_mdf.condition_scheduler
+
+    modeci_mdf.condition_scheduler.main(fi)
