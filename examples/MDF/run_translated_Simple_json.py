886c022fa3fa42fc53c5b6abb97a32e419e7897b
Shanka Subhra Mondal <smondal@griffiths-gpu02.pni.princeton.edu>
2021-07-26 22:11:10 -0400

    changing sys run call

--- examples/MDF/run_translated_Simple_json.py
+++ examples/MDF/run_translated_Simple_json.py
@@ -14,23 +14,22 @@ import sys
 
 def main():
 
-    parser = argparse.ArgumentParser(description=' Running the translator to stateful parameters')
-    parser.add_argument('--dt', default=5e-05, type=float,  help='time increment')
-    parser.add_argument('--run', default=False, type=bool,  help='Run the graph')
+    # parser = argparse.ArgumentParser(description=' Running the translator to stateful parameters')
+    # parser.add_argument('--dt', default=5e-05, type=float,  help='time increment')
+    # parser.add_argument('--run', default=False, type=bool,  help='Run the graph')
 
+    dt = 0.1
 
-
-    args = parser.parse_args()
-    print(args)
+    # args = parser.parse_args()
+    # print(args)
     file_path = 'Simple.json'
-    data = convert_states_to_stateful_parameters(file_path, args.dt)
+    data = convert_states_to_stateful_parameters(file_path, dt)
     # print(data)
     with open('Translated_'+ file_path, 'w') as fp:
         json.dump(data, fp,  indent=4)
 
 
-    if args.run:
-
+    if "-run" in sys.argv:
         f = open(file_path)
         data = json.load(f)
         filtered_list = ['parameters','functions', 'states','output_ports','input_ports','notes']
