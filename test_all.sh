2c4bf2817f5cadb5e52469123ef78bf2c119caf0
Padraig Gleeson <p.gleeson@gmail.com>
2021-10-18 14:46:17 +0100

    importer -> exporter in scripts

--- test_all.sh
+++ test_all.sh
@@ -21,32 +21,32 @@ python abc_conditions.py -run
 
 ## Test exporting to NeuroML
 
-python -m modeci_mdf.interfaces.neuroml.importer Simple.json -run
-python -m modeci_mdf.interfaces.neuroml.importer ABCD.json -run
-python -m modeci_mdf.interfaces.neuroml.importer States.json -run
+python -m modeci_mdf.interfaces.neuroml.exporter Simple.json -run
+python -m modeci_mdf.interfaces.neuroml.exporter ABCD.json -run
+python -m modeci_mdf.interfaces.neuroml.exporter States.json -run
 
 
 ## Test exporting to graphs via GraphViz
 
-python -m modeci_mdf.interfaces.graphviz.importer Simple.json 1 -noview
+python -m modeci_mdf.interfaces.graphviz.exporter Simple.json 1 -noview
 mv simple_example.gv.png images/simple.png
-python -m modeci_mdf.interfaces.graphviz.importer Simple.json 3 -noview
+python -m modeci_mdf.interfaces.graphviz.exporter Simple.json 3 -noview
 mv simple_example.gv.png images/simple_3.png
-python -m modeci_mdf.interfaces.graphviz.importer ABCD.json 1 -noview
+python -m modeci_mdf.interfaces.graphviz.exporter ABCD.json 1 -noview
 mv abcd_example.gv.png images/abcd.png
-python -m modeci_mdf.interfaces.graphviz.importer ABCD.json 3 -noview
+python -m modeci_mdf.interfaces.graphviz.exporter ABCD.json 3 -noview
 mv abcd_example.gv.png images/abcd_3.png
-python -m modeci_mdf.interfaces.graphviz.importer Arrays.json 3 -noview
+python -m modeci_mdf.interfaces.graphviz.exporter Arrays.json 3 -noview
 mv array_example.gv.png images/arrays.png
-python -m modeci_mdf.interfaces.graphviz.importer States.yaml 3 -noview
+python -m modeci_mdf.interfaces.graphviz.exporter States.yaml 3 -noview
 mv state_example.gv.png images/states.png
-python -m modeci_mdf.interfaces.graphviz.importer abc_conditions.yaml 3 -noview
+python -m modeci_mdf.interfaces.graphviz.exporter abc_conditions.yaml 3 -noview
 mv abc_conditions_example.gv.png images/abc_conditions.png
 
 ## Test regenerating NeuroML
 
 cd ../../examples/NeuroML
-./regenerateAndTest.sh
+./regenerateAndTest.sh -nogui
 
 ## Test ONNX examples
 
