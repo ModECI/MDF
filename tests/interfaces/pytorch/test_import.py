b40b80e41396817d0d58e0a102278e98d690fce9
David Turner <davidt0x@gmail.com>
2022-08-13 00:21:05 -0400

    Refactor tests for torchvision models.
    @parikshit14, I changed some of your code to remove a lot of duplication.
    test_import.py under pytorch now enumerates all models in torchvision and
    creates a test for each different type of model. All the models have the
    same interface so we can consolidate the testing code into a parameterized
    pytest.
    
    We have 5 out of 20 tests failing still, lets see if we can track down why
    each of these models is failing to run in the execution engine.

--- tests/interfaces/pytorch/test_import.py
+++ tests/interfaces/pytorch/test_import.py
@@ -2,349 +2,111 @@
 Tests for importing of PyTorch models into MDF. These tests use a lot of fixtures for models setup in ./conftest.py
 """
 import pytest
+import inspect
 import numpy as np
 
-try:
-    import torch
-    import torch.nn as nn
+from modeci_mdf.mdf import Model
+from modeci_mdf.execution_engine import EvaluableGraph
+from modeci_mdf.interfaces.pytorch import pytorch_to_mdf
 
-    torch.use_deterministic_algorithms(True)
-    torch.backends.cudnn.deterministic = True
+import torch
+import torchvision.models as models
 
-    from modeci_mdf.interfaces.pytorch import pytorch_to_mdf
 
-except ModuleNotFoundError:
-    pytest.mark.skip(
-        "Skipping PyTorch interface tests because pytorch is not installed."
+def _get_torchvision_models():
+    """
+    Get all the backbone models in torch vision, suprised there is no function to do this in torchvision.
+    """
+    models_to_test = []
+    model_classes = set()
+    for model_name, model in models.__dict__.items():
+        try:
+            params = inspect.signature(model).parameters
+
+            # Get the model class that this construction function returns. To cut down on tests,
+            # lets only test one version of each model.
+            return_type = inspect.signature(model).return_annotation
+
+            if (
+                "weights" in params
+                or "pretrained" in params
+                and return_type not in model_classes
+            ):
+                models_to_test.append(model)
+                if return_type:
+                    model_classes.add(return_type)
+
+        except TypeError:
+            continue
+
+    # New API for specifying pretrained=False is weughts=None. pretrained keyword
+    # will be removed soon. This handles that for all models depending on PyTorch
+    # version.
+    is_new_weights_api = "weights" in inspect.signature(models.resnet18).parameters
+    model_weights_spec = (
+        {"weights": None} if is_new_weights_api else {"pretrained": False}
     )
 
+    return [(model, model(**model_weights_spec)) for model in models_to_test]
 
-from modeci_mdf.execution_engine import EvaluableGraph
-from modeci_mdf.utils import load_mdf_json
+
+def _run_and_check_model(model, input=None):
+    """
+    Helper function that runs a complete set of tests on a model
+        - Runs the model in PyTorch to get expected output.
+        - Converts model to MDF and runs in Python execution engine.
+        - Compares the results.
+    """
+
+    # Create some test inputs for the model
+    if input is None:
+        input = torch.zeros((1, 3, 224, 224))
+
+    # Get rid of randomization due to Dropout
+    model.eval()
+
+    with torch.no_grad():
+        # Run the model once to get some ground truth output (from PyTorch)
+        output = model(input).detach().numpy()
+
+    # Convert to MDF
+    mdf_model, params_dict = pytorch_to_mdf(
+        model=model,
+        args=input,
+        trace=True,
+    )
+    # Get the graph
+    mdf_graph = mdf_model.graphs[0]
+    params_dict["input1"] = input.numpy()
+
+    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
+
+    eg.evaluate(initializer=params_dict)
+
+    output_mdf = eg.output_enodes[0].get_output()
+    assert np.allclose(
+        output,
+        output_mdf,
+    )
+
+    # Convert to JSON and back
+    mdf_model2 = Model.from_json(mdf_model.to_json())
+
+
+@pytest.mark.parametrize("model_init, model", _get_torchvision_models())
+def test_torchvision_models(model_init, model):
+    """Test importing the PyTorch model into MDF, executing in execution engine"""
+    _run_and_check_model(model)
 
 
 def test_simple_convolution(simple_convolution_pytorch):
     """Test a simple convolution neural network model"""
-    x = torch.zeros((1, 1, 28, 28))
-    ebv_output = torch.zeros((1,))
-
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = simple_convolution_pytorch(x).detach().numpy()
-
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=simple_convolution_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-    # params_dict["input2"] = ebv_output.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(output, output_mdf)
+    _run_and_check_model(simple_convolution_pytorch, torch.zeros((1, 1, 28, 28)))
 
 
 def test_convolution(convolution_pytorch):
     """Test a convolution neural network with more layers"""
-    x = torch.zeros((1, 1, 28, 28))
-    ebv_output = torch.zeros((10,))
-
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = convolution_pytorch(x).detach().numpy()
-
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=convolution_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-    # params_dict["input2"] = ebv_output.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-    )
-
-
-def test_vgg16(vgg16_pytorch):
-    """Test a dummy vgg16 model"""
-    # changed import call
-
-    # Create some test inputs for the model
-    x = torch.zeros((1, 3, 224, 224))
-    ebv_output = torch.zeros((1,))
-
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = vgg16_pytorch(x).detach().numpy()
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=vgg16_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-    # params_dict["input2"] = ebv_output.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-    )
-
-
-def test_vgg19(vgg19_pytorch):
-    """Test a dummy vgg19 model"""
-    # changed import call
-
-    # Create some test inputs for the model
-    x = torch.zeros((1, 3, 224, 224))
-    # Get rid of randomization due to Dropout
-    vgg19_pytorch.eval()
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = vgg19_pytorch(x).detach().numpy()
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=vgg19_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-    )
-
-
-def test_resnet18(resnet18_pytorch):
-    """Test a standard resnet model imported from PyTorch"""
-    # changed import call
-
-    x = torch.rand((5, 3, 224, 224))
-
-    # Run the model once to get some ground truth output (from PyTorch)
-    with torch.no_grad():
-        output = resnet18_pytorch(x)
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=resnet18_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-    # params_dict["input2"] = ebv_output.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-        # We need to compare results with an epsilon, I think this is related to some ONNX warnings:
-        #    Warning: ONNX Preprocess - Removing mutation from node aten::add_ on block input:
-        #    'bn1.num_batches_tracked'. This changes graph semantics.
-        atol=1e-5,
-    )
-
-
-def test_mobilenetv2(mobilenetv2_pytorch):
-    """Test a standard mobilenetv2 model"""
-
-    # Create some test inputs for the model
-    x = torch.zeros((1, 3, 224, 224))
-
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = mobilenetv2_pytorch(x)
-    # with torch.no_grad():
-    #     output = mobilenetv2_pytorch(x)
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=mobilenetv2_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-        # We need to compare results with an epsilon, I think this is related to some ONNX warnings:
-        #    Warning: ONNX Preprocess - Removing mutation from node aten::add_ on block input:
-        #    'bn1.num_batches_tracked'. This changes graph semantics.
-        atol=1e-5,
-    )
-
-
-def test_shufflenetv2(shufflenetv2_pytorch):
-    """Test a standard shufflenet_v2 model"""
-
-    # Create some test inputs for the model
-    x = torch.zeros((1, 3, 224, 224))
-
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = shufflenetv2_pytorch(x).detach().numpy()
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=shufflenetv2_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-    )
-
-
-def test_resNext(resNext_pytorch):
-    """Test a standard ResNext model"""
-
-    # Create some test inputs for the model
-    x = torch.zeros((1, 3, 224, 224))
-
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = resNext_pytorch(x).detach().numpy()
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=resNext_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-    )
-
-
-def test_squeezeNet(squeezeNet_pytorch):
-    """Test a standard SqueezeNet model"""
-
-    # Create some test inputs for the model
-    x = torch.zeros((1, 3, 224, 224))
-    # Get rid of randomization due to Dropout
-    squeezeNet_pytorch.eval()
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = squeezeNet_pytorch(x).detach().numpy()
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=squeezeNet_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-    )
-
-
-def test_mnasNet(mnasNet_pytorch):
-    """Test a standard MNASNet model"""
-
-    # Create some test inputs for the model
-    x = torch.zeros((1, 3, 224, 224))
-    # Get rid of randomization due to Dropout
-    mnasNet_pytorch.eval()
-    # Run the model once to get some ground truth output (from PyTorch)
-    output = mnasNet_pytorch(x).detach().numpy()
-
-    # Convert to MDF
-    mdf_model, params_dict = pytorch_to_mdf(
-        model=mnasNet_pytorch,
-        args=(x),
-        trace=True,
-    )
-    # Get the graph
-    mdf_graph = mdf_model.graphs[0]
-    params_dict["input1"] = x.numpy()
-
-    eg = EvaluableGraph(graph=mdf_graph, verbose=False)
-
-    eg.evaluate(initializer=params_dict)
-
-    output_mdf = eg.output_enodes[0].get_output()
-    assert np.allclose(
-        output,
-        output_mdf,
-    )
-
-
-def _check_model(mdf_model):
-    """A helper function to JIT compile a function or torch.nn.Module into Torchscript and convert to MDF and check it"""
-
-    # Generate JSON
-    mdf_model.to_json_file("test.json")
-
-    # Load the JSON
-    load_mdf_json("test.json")
+    _run_and_check_model(convolution_pytorch, torch.zeros((1, 1, 28, 28)))
 
 
 def test_simple_module():
@@ -360,7 +122,7 @@ def test_simple_module():
         use_onnx_ops=True,
     )
 
-    _check_model(mdf_model)
+    mdf_model2 = Model.from_json(mdf_model.to_json())
 
 
 def test_simple_function():
@@ -375,7 +137,7 @@ def test_simple_function():
         use_onnx_ops=True,
     )
 
-    _check_model(mdf_model)
+    mdf_model2 = Model.from_json(mdf_model.to_json())
 
 
 def test_inception(inception_model_pytorch):
@@ -411,6 +173,4 @@ def test_inception(inception_model_pytorch):
         output_mdf,
     )
 
-
-if __name__ == "__main__":
-    test_simple_module()
+    mdf_model2 = Model.from_json(mdf_model.to_json())
