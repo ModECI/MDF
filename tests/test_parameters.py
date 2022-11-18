acf88e9cd6a2dbab549ecc5b8e5412df97535e09
Padraig Gleeson <p.gleeson@gmail.com>
2022-11-18 17:25:56 +0000

    Regenerated docs; fix formatting

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -11,19 +11,19 @@ from modeci_mdf.execution_engine import (
 @pytest.mark.parametrize(
     "value, parameters, result",
     [
-        ('3+4', {}, 7),
-        ('3+x', {'x':3}, 6.),
-        ('[x,4]', {'x':3}, [3,4]),
-        ([1,2], {'x':3}, [1,2]),
-        (numpy.array([1,2]), {'x':2}, numpy.array([1,2])),
+        ("3+4", {}, 7),
+        ("3+x", {"x": 3}, 6.0),
+        ("[x,4]", {"x": 3}, [3, 4]),
+        ([1, 2], {"x": 3}, [1, 2]),
+        (numpy.array([1, 2]), {"x": 2}, numpy.array([1, 2])),
     ],
 )
 def test_evaluable_parameter(value, parameters, result):
 
-    p = Parameter(id='p1',value=value)
+    p = Parameter(id="p1", value=value)
     ep = EvaluableParameter(p)
     evl = ep.evaluate(parameters)
-    print('Evaluated %s with %s to be: %s'%(value, parameters, evl))
+    print("Evaluated %s with %s to be: %s" % (value, parameters, evl))
 
-    assert numpy.all(evl==result)
-    assert numpy.all(ep.get_current_value(parameters)==result)
+    assert numpy.all(evl == result)
+    assert numpy.all(ep.get_current_value(parameters) == result)
