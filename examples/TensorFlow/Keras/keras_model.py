5bc9bdf91aa51509f606d630c482fd29755e4bdd
Kusanele Mpofu <mpofukusanele@gmail.com>
2023-02-02 19:09:40 +0200

    Modified keras machine leaning model

--- examples/TensorFlow/Keras/keras_model.py
+++ examples/TensorFlow/Keras/keras_model.py
@@ -1,183 +1,57 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 import tensorflow as tf
-from tensorflow.keras import layers
 
-# ## Loading MNIST
-# In[ ]:
+# tf.__version__
+import matplotlib.pyplot as plt
+import numpy as np
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train = x_train / 255
-x_test = x_test / 255
+print("Loading data")
+mnist = tf.keras.datasets.mnist  # 28*28 images of handwritten digits (0-9)
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# In[3]:
+# data Normalizations ( scalling numbers between 0 and 1)
+print("Normalizing data")
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+# Build Model
+print("Building the Model")
+kr_model = tf.keras.models.Sequential()
+kr_model.add(tf.keras.layers.Flatten())  # input layer
 
-print(x_train.shape)
-print(y_train.shape)
+kr_model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # hidden layers
+kr_model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+kr_model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  # output layer
 
-
-# In[4]:
-
-
-plt.imshow(x_train[0], cmap="Greys")
-
-
-# ## Training with one-hot labels
-
-# In[5]:
-
-
-model_lr = tf.keras.models.Sequential(
-    [
-        layers.Input(x_train.shape[1:]),
-        layers.Flatten(),
-        layers.Dense(10, activation="softmax"),
-    ]
-)
-model_lr.compile(
-    optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
-)
-model_lr.summary()
-
-
-# In[6]:
-
-
-y_onehot_train = tf.one_hot(y_train, 10)
-model_lr.fit(x_train, y_onehot_train)
-
-
-# ## Training with sparse labels
-
-# In[7]:
-
-
-model_lr = tf.keras.models.Sequential(
-    [
-        layers.Input(x_train.shape[1:]),
-        layers.Flatten(),
-        layers.Dense(10, activation="softmax"),
-    ]
-)
-model_lr.compile(
+# Compile Model
+print("Compiling the model")
+kr_model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
-model_lr.summary()
+
+# train the model
+print("Training the Model")
+kr_model.fit(x_train, y_train, epochs=3)
+
+# save model
+print("Saving the Model")
+kr_model.save("num_reader.model")
+
+# loadthe model above
+# new_model = tf.keras.models.load_model("num_reader.model")
 
 
-# In[8]:
+# check if the model actually generalize the data.
+print("Accuracy of our model is:")
+val_loss, val_acc = kr_model.evaluate(x_test, y_test)
+print(val_loss, val_acc)
 
+# predict example for index 0
+print("Predict value at index 0:")
+predictions = kr_model.predict([x_test])
+print("The predicted number at index 0 is", np.argmax(predictions[0]))
 
-# model_lr.fit(x_train, y_train)
-
-
-# In[9]:
-
-
-history_lr = model_lr.fit(
-    x_train,
-    y_train,
-    epochs=10,
-    batch_size=128,
-    validation_data=(x_test, y_test),
-    verbose=False,
-)
-
-
-# ## Review Traning Results
-
-# In[11]:
-
-
-plt.plot(history_lr.history["loss"], label="train")
-plt.plot(history_lr.history["val_loss"], label="val")
-plt.ylabel("loss")
-plt.legend()
-plt.show()
-
-plt.plot(history_lr.history["accuracy"], label="train")
-plt.plot(history_lr.history["val_accuracy"], label="val")
-plt.ylabel("accuracy")
-plt.legend()
-plt.show()
-
-
-# In[12]:
-
-
-model_lr.evaluate(x_test, y_test)
-
-
-# In[16]:
-
-
-probs = model_lr.predict(x_test[:5])
-preds = np.argmax(probs, axis=1)
-for i in range(5):
-    print(probs[i], " => ", preds[i])
-    plt.imshow(x_test[i], cmap="Greys")
-    plt.show()
-
-
-# In[18]:
-
-
-model_lr.predict(x_test[18].reshape(1, 28, 28))
-
-
-# In[19]:
-
-
-model_lr.predict(x_test[18:19])
-
-
-# ## Adding Model Complexity
-
-# In[20]:
-
-
-model_mlp = tf.keras.models.Sequential(
-    [
-        layers.Input(x_train.shape[1:]),
-        layers.Flatten(),
-        layers.Dense(64, activation="elu"),
-        layers.Dense(64, activation="elu"),
-        layers.Dense(10, activation="softmax"),
-    ]
-)
-model_mlp.compile(
-    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-)
-model_mlp.summary()
-
-
-# In[21]:
-
-
-history_mlp = model_mlp.fit(
-    x_train,
-    y_train,
-    epochs=10,
-    batch_size=128,
-    validation_data=(x_test, y_test),
-    verbose=False,
-)
-
-
-# In[22]:
-
-
-plt.plot(history_mlp.history["loss"], label="train")
-plt.plot(history_mlp.history["val_loss"], label="val")
-plt.ylabel("loss")
-plt.legend()
-plt.show()
-
-plt.plot(history_mlp.history["accuracy"], label="train")
-plt.plot(history_mlp.history["val_accuracy"], label="val")
-plt.ylabel("accuracy")
-plt.legend()
+# print the actauly value at that index
+print("The actually number at index zero is (see the image below):")
+plt.imshow(x_test[0])
 plt.show()
