889712116d39da1fd7062daf16d10869e2c5803c
pgleeson <p.gleeson@gmail.com>
2023-05-25 18:35:16 +0100

    Improved handling of spike transfers

--- examples/NeuroML/PyNN/regenerateAndTest.sh
+++ examples/NeuroML/PyNN/regenerateAndTest.sh
@@ -15,9 +15,24 @@ python Generate.py -one -mdf
 python RunInMDF.py OneCell.mdf.json -nogui
 
 
+####  Generate HH example
+python Generate.py -hh -nml
+pynml -validate HH.net.nml
+
+python Generate.py -hh -mdf
+python RunInMDF.py HH.mdf.json -nogui
+
+
 ####  Generate multiple input example
 python Generate.py -input_weights -nml
 pynml -validate InputWeights.net.nml
 
 python Generate.py -input_weights -mdf
 python RunInMDF.py InputWeights.mdf.json -nogui
+
+####  Generate simple net example
+python Generate.py -simple_net -nml
+pynml -validate SimpleNet.net.nml
+
+python Generate.py -simple_net -mdf
+python RunInMDF.py SimpleNet.mdf.json -nogui
