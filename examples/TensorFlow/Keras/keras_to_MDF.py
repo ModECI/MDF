b5c3cc8bed67d5bd0d51d4874dca2c4bb481dcb2
Onoyiza <odpizzy1@gmail.com>
2023-07-31 18:17:37 +0100

    pre-commit script for keras_to_mdf function call

--- examples/TensorFlow/Keras/keras_to_MDF.py
+++ examples/TensorFlow/Keras/keras_to_MDF.py
@@ -6,7 +6,7 @@ from modelspec.utils import _val_info
 from modeci_mdf.execution_engine import EvaluableGraph
 
 # load the keras model
-model =  tf.keras.models.load_model("kr_N_model.h5")
+model = tf.keras.models.load_model("kr_N_model.h5")
 
 # get one of the test images from the mnnist test dataset
 _, (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -20,25 +20,23 @@ one_x_test = one_x_test.reshape(1, 28, 28)
 output = model.predict(one_x_test)
 
 # Convert the Keras model to MDF
-mdf_model, params_dict = keras_to_mdf(
-    model=model,
-    args=one_x_test
-)
+mdf_model, params_dict = keras_to_mdf(model=model, args=one_x_test)
 
 # Get mdf graph
 mdf_graph = mdf_model.graphs[0]
 
 # visualize mdf graph-image
 mdf_model.to_graph_image(
-        engine="dot",
-        output_format="png",
-        view_on_render=False,
-        level=1,
-        filename_root="keras_to_MDF",
-        is_horizontal=True,
-        solid_color=True
+    engine="dot",
+    output_format="png",
+    view_on_render=False,
+    level=1,
+    filename_root="keras_to_MDF",
+    is_horizontal=True,
+    solid_color=True,
 )
 from IPython.display import Image
+
 Image(filename="Keras_to_MDF.png")
 
 # Evaluate the model via the MDF scheduler
