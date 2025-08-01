a5e16cea76e320a579a3f25c39520b602cd38ad8
David Turner <dmturner@princeton.edu>
2025-08-01 16:57:31 -0400

    Fix weird import for keras_model.py.
    I think the read and exec'ing of keras_model.py
    in keras_to_MDF.py might be causing some deadlocks
    in tensorflow on macos. Testing ...

--- examples/TensorFlow/Keras/MNIST/keras_model.py
+++ examples/TensorFlow/Keras/MNIST/keras_model.py
@@ -20,67 +20,73 @@ import numpy as np
 # based on https://keras.io/examples/keras_recipes/reproducibility_recipes/
 from keras.utils import set_random_seed
 
-set_random_seed(1234)
-tf.config.experimental.enable_op_determinism()
 
-print("Loading data")
-mnist = tf.keras.datasets.mnist  # 28*28 images of handwritten digits (0-9)
+def run():
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    set_random_seed(1234)
+    tf.config.experimental.enable_op_determinism()
 
-# data Normalizations ( scalling numbers between 0 and 1)
-print("Normalizing data")
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+    print("Loading data")
+    mnist = tf.keras.datasets.mnist  # 28*28 images of handwritten digits (0-9)
 
-# Build Model
-print("Building the Model")
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten())  # input layer
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # hidden layers
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  # output layer
+    # data Normalizations ( scalling numbers between 0 and 1)
+    print("Normalizing data")
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-# Compile Model
-print("Compiling the model")
-model.compile(
-    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-)
+    # Build Model
+    print("Building the Model")
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())  # input layer
 
-# train the model
-print("Training the Model")
-model.fit(x_train, y_train, epochs=3)
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # hidden layers
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  # output layer
 
-# loadthe model above
-# new_model = tf.keras.models.load_model("num_reader.model")
+    # Compile Model
+    print("Compiling the model")
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
 
+    # train the model
+    print("Training the Model")
+    model.fit(x_train, y_train, epochs=3)
 
-# check if the model actually generalize the data.
-print("Accuracy of our model is:")
-val_loss, val_acc = model.evaluate(x_test, y_test)
-print(val_loss, val_acc)
+    # load the model above
+    # new_model = tf.keras.models.load_model("num_reader.model")
 
-# Print summary
-model.summary()
+    # check if the model actually generalize the data.
+    print("Accuracy of our model is:")
+    val_loss, val_acc = model.evaluate(x_test, y_test)
+    print(val_loss, val_acc)
 
-# plot the model
-print("Plotting the model")
-plot_model(model, to_file="model_plot.png", show_shapes=True, show_layer_names=True)
+    # Print summary
+    model.summary()
 
-
-# Saving model in h5 for Ploting Nuetron visual
-model.save("kr_N_model.keras")
+    # plot the model
+    print("Plotting the model")
+    plot_model(model, to_file="model_plot.png", show_shapes=True, show_layer_names=True)
 
 
-# predict example for index 0
-print("Predict value at index 0:")
-predictions = model.predict([x_test])
-print("The predicted number at index 0 is", np.argmax(predictions[0]))
+    # Saving model in h5 for Ploting Nuetron visual
+    model.save("kr_N_model.keras")
 
-# print the actual value at that index
-print("The actually number at index zero is (see the image below):")
-plt.imshow(x_test[0])
+    # predict example for index 0
+    print("Predict value at index 0:")
+    predictions = model.predict([x_test])
+    print("The predicted number at index 0 is", np.argmax(predictions[0]))
 
-if not "-nogui" in sys.argv:
-    plt.show()
+    # print the actual value at that index
+    print("The actually number at index zero is (see the image below):")
+    plt.imshow(x_test[0])
+
+    if not "-nogui" in sys.argv:
+        plt.show()
+
+    return model
+
+if __name__ == "__main__":
+    run()
